e65e9981b9e99aabfc907c0d721501a1f5601095
AC75095615 <thomas.mathieu2@soprasteria.com>
2020-09-06 17:47:31 +0200

    architecture

---
+++ Z-Project-Back/src/main/java/com/thmathieu/zproject/ZProjectBackApplication.java
@@ -0,0 +1,13 @@
+package com.thmathieu.zproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZProjectBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZProjectBackApplication.class, args);
+	}
+
+}
---
+++ Z-Project-Back/src/test/java/com/thmathieu/zproject/ZProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thmathieu.zproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
